9b4178ae77c2887a1055d3ee2b57bd4812a6fa11
Okezie Emeka <okezieemeka949@gmail.com>
2021-07-25 04:45:55 +0100

    Completed the navbar of this application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from './pages/Home/Home';
 
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h2> Netflix UI Cone </h2>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
+import './style.scss'
+
+const Navbar = () => {
+
+    const [ isScroll , setIsScroll ] = useState(false)
+
+    window.onscroll = () => {
+        setIsScroll(window.pageYOffset === 0 ? false : true );
+        return () => (window.onscroll = null);
+    }
+
+    return (
+        <div className={isScroll ? "navbar scrolled" : "navbar"}>
+            <div className="container">
+                <div className="container__left">
+                    <img 
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+                        alt=""
+                        className="logo_img"
+                    />
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+                <div className="container__right">
+                    <Search className="container__right--icons" />
+                    <span>KID</span>
+                    <Notifications className="container__right--icons" />
+                    <img 
+                        className=""
+                        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+                        alt=""
+                    />
+                    <div className="container__right--profile">
+                        <ArrowDropDown className="container__right--icons" />
+                        <div className="container__right--profileOptions">
+                            <span>Settings</span>
+                            <span>Log out</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
